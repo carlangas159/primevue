4d5cf2b4f047c06ca0ad14614d2ec7fbb8519512
Carlos Alvarado <alvaradocarlo@gmail.com>
2023-07-21 15:35:15 -0500

    Build

--- pages/tabmenu/Documentation.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="tabmenudemo-content">
-        <h3>Documentation Component Content</h3>
-    </div>
-</template>
-
-<script>
-export default {};
-</script>
--- pages/tabmenu/Settings.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="tabmenudemo-content">
-        <h3>Settings Component Content</h3>
-    </div>
-</template>
-
-<script>
-export default {};
-</script>
